ad8c35912530e091421af73b6ae53030bf4d534a
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-08 19:28:28 +0300

    auth1

--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function auth() {
   return (
-    <div>auth</div>
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
+      <div className='bg-black h-full w-full bg-opacity-45'>
+
+        <nav className='px-12 py-6'>
+          <img src="/images/logo.png" className='h-12'/>
+        </nav>
+
+        <div className='flex justify-center'>
+          <div className='bg-black bg-opacity-85 px-20 py-20 self-center mt-2 lg:w-2/5 rounded-xl w-full'>
+            <h2 className='text-white text-5xl mb-8 font-semibold'>Register</h2>
+
+            <div className='flex flex-col gap-4'>
+
+
+            </div>
+
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+      </div>
   )
 }
 
