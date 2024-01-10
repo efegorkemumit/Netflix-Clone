546cabfd327ed72b288549273f43f38e469d4e55
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-10 16:15:06 +0300

    NextAuth

---
+++ netflix-clone/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,71 @@
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+import prismadb from '@/libs/prismadb'
+import {compare} from 'bcrypt'
+import { signIn } from "next-auth/react";
+import { debug } from "console";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+export const authOptions : AuthOptions = {
+    providers:[
+
+        Credentials({
+            id:'credentials',
+            name:'Credentials',
+            credentials:{
+                email:{
+                    label:'Email',
+                    type:'text'
+                },
+                password:{
+                    label:'Password',
+                    type:'password'
+                }
+            },
+            async authorize(credentials){
+                if(!credentials?.email || !credentials.password){
+                    throw new Error('Email or Password required')
+                }
+
+                const user = await prismadb.user.findUnique({
+                    where{
+                        email:credentials.email
+                    }
+                });
+
+                if(!user || !user.hashedPassword){
+                    throw new Error('Email does not exist')
+                }
+
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
+                if(!isCorrectPassword){
+                    throw new Error('Incorrect Password')
+                }
+
+                return  user;
+
+
+
+
+            }
+
+
+        })
+
+
+    ],
+    pages:{
+        signIn:'/auth'
+    },
+    debug.process.env.NODE_ENV==='development',
+    adapter:PrismaAdapter(prismadb),
+    session :{ stragety : 'jwt'},
+    jwt:{
+        secret: process.env.NEXTAUTH_JWT_SECRET,
+    },
+    secret:process.env.NEXTAUTH_SECRET
+};
+
+export default NextAuth(authOptions);
