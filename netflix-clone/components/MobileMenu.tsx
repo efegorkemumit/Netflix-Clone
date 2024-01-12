ff1c055b6c5a213e2e557c68ba3c5a142444dead
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-12 18:20:57 +0300

    nav 2

--- netflix-clone/components/MobileMenu.tsx
+++ netflix-clone/components/MobileMenu.tsx
@@ -10,7 +10,27 @@ const MobileMenu: React.FC<mobilProp>= ({visible})=> {
         return null;
     }
   return (
-    <div>MobileMenu</div>
+    <div className=
+    'bg-gray-950 w-48 absolute rounded-lg top-8 border-2 left-0 py-5 flex-col border-gray-800 flex'>
+        <div className='flex flex-col gap-4'>
+            <div className='px-3 text-center text-white hover:underline'>
+                Home
+            </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                Film
+            </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                New & Popular
+            </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                My List
+            </div>
+            <div className='px-3 text-center text-white hover:underline'>
+                Browse by Langugaes
+            </div>
+
+        </div>
+     </div>
   )
 }
 
--- netflix-clone/components/Navbar.tsx
+++ netflix-clone/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import { BellIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
+import React, { use, useCallback, useState } from 'react'
+import { BellIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import NavItem from './NavItem'
+import MobileMenu from './MobileMenu';
 
 
 function Navbar() {
+
+    const [MobileMenuItem, setMobileMenu]=useState(false);
+
+    const toogleMobileMenu = useCallback(()=>{
+        setMobileMenu((current)=> !current)
+    })
   return (
     <nav className='w-full fixed z-20'>
         <div className='px-4 py-6 flex flex-row transition'>
@@ -16,13 +23,30 @@ function Navbar() {
                 <NavItem name='My List' ></NavItem>
                 <NavItem name='Browse My Languages' ></NavItem>
             </div>
-            <div className='lg:hidden flex flex-row items-center gap-2 ml-6'>
+            <div onClick={toogleMobileMenu} className=' relative lg:hidden flex flex-row items-center gap-2 ml-6'>
                 <p className='text-white'>Browse</p>
                 <ChevronDownIcon className='w-5 text-white'>
                 </ChevronDownIcon>
-
+                <MobileMenu visible={MobileMenuItem}> </MobileMenu>
 
             </div>
+            <div className='flex flex-row ml-auto gap-7 items-center'>
+                <div className='cursor-pointer'>
+                <MagnifyingGlassIcon className='w-5 text-white'></MagnifyingGlassIcon>
+                </div>
+                
+                <div className='cursor-pointer'>
+                <BellIcon className='w-5 text-white'></BellIcon>
+                </div>
+
+                <div className='flex flex-row ml-auto gap-2 items-center'>
+                <div className='lg:h-8 lg:w-8 h-6 w-6 rounded-lg overflow-hidden'>
+                    <img src='/images/default-red.png'></img>
+                </div>
+               
+            </div>
+            </div>
+           
 
 
         </div>
