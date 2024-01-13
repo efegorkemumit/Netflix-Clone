3078644e2725c56bc29a6bc198514f324d4a4764
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-13 16:38:03 +0300

    movie list button

--- netflix-clone/components/MovieCard.tsx
+++ netflix-clone/components/MovieCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PlayIcon } from '@heroicons/react/24/solid'
+import { PlayIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
 
 interface MovieCartProp{
     data : MovieInterface;
@@ -28,9 +28,10 @@ const  MovieCard: React.FC<MovieCartProp>=({data})=> {
                 <div className='flex flex-row items-center gap-4'>
 
                 <div className='bg-white
-                 flex items-center justify-center rounded-full w-6 h-7 lg:w-8
+                 flex items-center justify-center rounded-full   w-7 h-7 lg:w-11 lg:h-11
                  transition 
                  border-2
+                 cursor-pointer
                  border-white
                  hover:border-neutral-300'>
                     <PlayIcon className='text-black w-4 lg:w-6'></PlayIcon>
@@ -38,6 +39,32 @@ const  MovieCard: React.FC<MovieCartProp>=({data})=> {
 
                 </div>
 
+                <div>
+                <PlayIcon className='text-black w-4 lg:w-6'></PlayIcon>
+
+                </div>
+
+                <div className='ml-auto  flex items-center justify-center rounded-full 
+                w-7 h-7 lg:w-11 lg:h-11
+                 transition 
+                 border-2
+                 cursor-pointer'>
+                <ChevronDownIcon className='text-white w-4 lg:w-6'></ChevronDownIcon>
+                </div>
+
+                </div>
+                <p className='text-green-500 text-sm font-semibold mt-4'> 
+                  New 
+                <span className='ml-2 text-white'>2024</span>
+                </p>
+
+                <div className='flex flex-row mt-4 gap-2 items-center'>
+                    <p className='text-white text-sm'> {data.duration}</p>
+
+                </div>
+
+                <div className='flex flex-row mt-4 gap-2 items-center'>
+                    <p className='text-white text-sm'> {data.genre}</p>
 
                 </div>
 
